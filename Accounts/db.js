4ac7241f8f3f9262cf2d3e33cb6a62057ab6e37d
unknown <vladancupric@gmail.com>
2021-08-08 22:03:02 +0200

    New commits

--- Accounts/db.js
+++ Accounts/db.js
@@ -1,23 +1,23 @@
-let db = [{
-    id:1,
-    name:"Ranko",
-    lastname:"Stajic",
-    email:"rankostajic@gmail.com",
-    phone:"0601531174"
-},
-{
-    id:2,
-    name:"Stefan",
-    lastname:"Kostov",
-    email:"stefankostov@gmail.com",
-    phone:"0643081165"
-},
-{
-    id:3,
-    name:"Dragan",
-    lastname:"Ikanovic",
-    email:"draganikanovic@gmail.com",
-    phone:"0643081166"
-}
-
+let db = [
+  {
+    id: 1,
+    name: "Ranko",
+    lastname: "Stajic",
+    email: "rankostajic@gmail.com",
+    phone: "0601531174",
+  },
+  {
+    id: 2,
+    name: "Stefan",
+    lastname: "Kostov",
+    email: "stefankostov@gmail.com",
+    phone: "0643081165",
+  },
+  {
+    id: 3,
+    name: "Dragan",
+    lastname: "Ikanovic",
+    email: "draganikanovic@gmail.com",
+    phone: "0643081166",
+  },
 ];
--- Accounts/main.js
+++ Accounts/main.js
@@ -1,25 +1,80 @@
 let accountsTableBody = document.querySelector("#accounts-table-body");
-let accountsViewBtn = document.querySelector('[href="accounts-view"]');
-let addAccountsViewBtn = document.querySelector('[href="add-account-view"]');
-let accountsView = document.querySelector('#accounts-view');
-let addAccountView = document.querySelector('#add-account-view');
+// let accountsViewBtn = document.querySelector('[href="accounts-view"]');
+// let addAccountsViewBtn = document.querySelector('[href="add-account-view"]');
+let allLinks = document.querySelectorAll(".nav-link");
+let accountsView = document.querySelector("#accounts-view");
+let addAccountView = document.querySelector("#add-account-view");
+let views = document.querySelectorAll(".view");
+let idInput = document.querySelector('[placeholder="id"]');
+let nameInput = document.querySelector('[placeholder="name"]');
+let lastNameInput = document.querySelector('[placeholder="lastname"]');
+let emailInput = document.querySelector('[placeholder="email"]');
+let phoneInput = document.querySelector('[placeholder="phone"]');
+let saveBtn = document.querySelector("#save");
 
+saveBtn.addEventListener("click", saveAccount);
+function saveAccount() {
+  const newAccount = {
+    id: idInput.value,
+    name: nameInput.value,
+    lastname: lastNameInput.value,
+    email: emailInput.value,
+    phone: phoneInput.value,
+  };
+  db.push(newAccount);
+  idInput.value ="";
+  nameInput.value="";
+  lastNameInput.value="";
+  emailInput.value="";
+  phoneInput.value="";
+  createAccountsTable();
+  showView("#accounts-view");
+}
+
+// iz nekog razloga mora da bude ovde inace ne povezuje sa db
 console.log(db);
-addAccountsViewBtn.addEventListener('click', function(e){
-    e.preventDefault();
-addAccountView.style.display ="block";
-accountsView.style.display ="none";
-});
 
+// // Add accounts button
+// addAccountsViewBtn.addEventListener('click', function(e){
+//     e.preventDefault();
+// addAccountView.style.display ="block";
+// accountsView.style.display ="none";
+// });
+
+// // Accounts button
+
+// accountsViewBtn.addEventListener('click', function(e){
+//     e.preventDefault();
+// // mora da se obrne redosled, ukoliko se samo prekopira i zameni ne radi
+// accountsView.style.display ="block";
+// addAccountView.style.display ="none";
+// });
+
+for (let i = 0; i < allLinks.length; i++) {
+  allLinks[i].addEventListener("click", showView);
+}
+
+function showView(e) {
+  for (let i = 0; i < views.length; i++) {
+    views[i].style.display = "none";
+  }
+
+  if (e instanceof Event) {
+    e.preventDefault();
+    let id = `#${this.getAttribute("href")}`;
+    document.querySelector(id).style.display = "block";
+  } else {
+    document.querySelector(e).style.display = "block";
+  }
+}
 
 createAccountsTable();
 
-
-function createAccountsTable(){
-    let htmlAccounts = '';
-    for(let i = 0; i<db.length;i++){
-        const account = db[i];
-        htmlAccounts +=`
+function createAccountsTable() {
+  let htmlAccounts = "";
+  for (let i = 0; i < db.length; i++) {
+    const account = db[i];
+    htmlAccounts += `
         <tr>
         <td>${account.id}</td>
         <td>${account.name}</td>
@@ -28,7 +83,6 @@ function createAccountsTable(){
         <td>${account.phone}</td>
     </tr>
     `;
-    }
-   accountsTableBody.innerHTML = htmlAccounts;
-
+  }
+  accountsTableBody.innerHTML = htmlAccounts;
 }
