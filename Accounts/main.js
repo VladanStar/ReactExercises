6d9560c497c4ef4fcc3fe148e72cc101bef8525f
unknown <vladancupric@gmail.com>
2021-08-08 19:13:39 +0200

    New commits react

--- Accounts/main.js
+++ Accounts/main.js
@@ -1,8 +1,22 @@
-let accountTableBody = document.querySelector('#accounts-table-body');
+let accountsTableBody = document.querySelector("#accounts-table-body");
+let accountsViewBtn = document.querySelector('[href="accounts-view"]');
+let addAccountsViewBtn = document.querySelector('[href="add-account-view"]');
+let accountsView = document.querySelector('#accounts-view');
+let addAccountView = document.querySelector('#add-account-view');
+
+console.log(db);
+addAccountsViewBtn.addEventListener('click', function(e){
+    e.preventDefault();
+addAccountView.style.display ="block";
+accountsView.style.display ="none";
+});
+
 
 createAccountsTable();
+
+
 function createAccountsTable(){
-    let htmlAccounts = "";
+    let htmlAccounts = '';
     for(let i = 0; i<db.length;i++){
         const account = db[i];
         htmlAccounts +=`
@@ -13,8 +27,8 @@ function createAccountsTable(){
         <td>${account.email}</td>
         <td>${account.phone}</td>
     </tr>
-    `
+    `;
     }
-   accountTableBody.innerHTML = htmlAccounts;
+   accountsTableBody.innerHTML = htmlAccounts;
 
 }
