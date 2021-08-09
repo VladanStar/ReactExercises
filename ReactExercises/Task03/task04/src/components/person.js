b9851ae0c66eb7a1787bf0d19f9b4fead8764276
unknown <vladancupric@gmail.com>
2021-08-09 17:35:56 +0200

    React app

---
+++ ReactExercises/Task03/task04/src/components/person.js
@@ -0,0 +1,10 @@
+import React from "react";
+const Person = ()=>{
+    return (<div>
+        <h3>Danilo</h3>
+        <h3>development</h3>
+        <h3>male</h3>
+    </div>
+    )
+}
+export default Person;
--- ReactExercises/Task03/task04/src/index.js
+++ ReactExercises/Task03/task04/src/index.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class App extends React.Component{
-  render(){
-    return(
-      <h1>App Component</h1>
-    )
-  }
-}
+import App from "./App"
 ReactDOM.render(
   <App />,
   document.querySelector('#app')
