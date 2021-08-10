f9719740221c2e71632b277f0ca4bda1d91e5198
unknown <vladancupric@gmail.com>
2021-08-10 12:48:46 +0200

    New commits css

--- ReactExercises/Task03/task04/src/components/person.js
+++ ReactExercises/Task03/task04/src/components/person.js
@@ -1,10 +1,30 @@
 import React from "react";
-const Person = ()=>{
-    return (<div>
-        <h3>Vladan</h3>
-        <h3>development</h3>
-        <h3>male</h3>
+const Person = () => {
+  // const myStyles ={
+  //     card: {
+  //         background:"lightyellow",
+  //         color:"lightblue",
+  //         fontSize:"25px",
+  //         textAlign:"center",
+  //         width: "400px"
+  //     }
+  //}
+  // return (<div style ={myStyles.card}>
+
+  return (
+    <div className="row">
+      <div className="card">
+        <div className="card-header">
+          <h3>Vladan</h3>
+        </div>
+        <div className="card-body">
+          <h3>development</h3>
+        </div>
+        <div className="card-footer">
+          <h3>male</h3>
+        </div>
+      </div>
     </div>
-    )
-}
+  );
+};
 export default Person;
