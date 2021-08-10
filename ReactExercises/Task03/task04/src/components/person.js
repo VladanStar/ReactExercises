06d20318adaa24b315d5bfa7376a4caa3ada3cd2
unknown <vladancupric@gmail.com>
2021-08-10 08:08:34 +0200

    New commits

--- ReactExercises/Task03/task04/src/components/person.js
+++ ReactExercises/Task03/task04/src/components/person.js
@@ -1,7 +1,7 @@
 import React from "react";
 const Person = ()=>{
     return (<div>
-        <h3>Danilo</h3>
+        <h3>Vladan</h3>
         <h3>development</h3>
         <h3>male</h3>
     </div>
